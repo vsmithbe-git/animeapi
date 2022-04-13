c85ef5b784e4163688e788c59f66f45752b4802e
vsmithbe-git <98296635+vsmithbe-git@users.noreply.github.com>
2022-04-13 02:36:39 -0500

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="bg-blue-800 text-white">	
+    <div class="container mx-auto flex justify-center items-center">
+       <h1 class="tracking-tight text-3xl font-thin py-8">Anime<span class="font-normal">Facts</span>
+        </h1>
+ 
+       
+       
+    </div>
+</div>
+
+</template>
